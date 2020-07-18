f30ef4bd313e87c7073104675a6f4e537f18fa24
Shivangi <shivangi.das7@gmail.com>
2020-07-18 14:59:09 +0100

    remove unnecessary data, add duplicate user

--- test/mock_data/mock_data.js
+++ test/mock_data/mock_data.js
@@ -1,86 +1,3 @@
-const inAndAroundLondonUsers = [
-  {
-    id: 266,
-    first_name: "Ancell",
-    last_name: "Garnsworthy",
-    email: "agarnsworthy7d@seattletimes.com",
-    ip_address: "67.4.69.137",
-    latitude: 51.6553959,
-    longitude: 0.0572553
-  },
-  {
-    id: 322,
-    first_name: "Hugo",
-    last_name: "Lynd",
-    email: "hlynd8x@merriam-webster.com",
-    ip_address: "109.0.153.166",
-    latitude: 51.6710832,
-    longitude: 0.8078532
-  },
-  {
-    id: 554,
-    first_name: "Phyllys",
-    last_name: "Hebbs",
-    email: "phebbsfd@umn.edu",
-    ip_address: "100.89.186.13",
-    latitude: 51.5489435,
-    longitude: 0.3860497
-  },
-  {
-    id: 135,
-    first_name: "Mechelle",
-    last_name: "Boam",
-    email: "mboam3q@thetimes.co.uk",
-    ip_address: "113.71.242.187",
-    latitude: -6.5115909,
-    longitude: 105.652983
-  },
-  {
-    id: 396,
-    first_name: "Terry",
-    last_name: "Stowgill",
-    email: "tstowgillaz@webeden.co.uk",
-    ip_address: "143.190.50.240",
-    latitude: -6.7098551,
-    longitude: 111.3479498
-  },
-  {
-    id: 520,
-    first_name: "Andrew",
-    last_name: "Seabrocke",
-    email: "aseabrockeef@indiegogo.com",
-    ip_address: "28.146.197.176",
-    latitude: "27.69417",
-    longitude: "109.73583"
-  },
-  {
-    id: 658,
-    first_name: "Stephen",
-    last_name: "Mapstone",
-    email: "smapstonei9@bandcamp.com",
-    ip_address: "187.79.141.124",
-    latitude: -8.1844859,
-    longitude: 113.6680747
-  },
-  {
-    id: 688,
-    first_name: "Tiffi",
-    last_name: "Colbertson",
-    email: "tcolbertsonj3@vimeo.com",
-    ip_address: "141.49.93.0",
-    latitude: 37.13,
-    longitude: -84.08
-  },
-  {
-    id: 794,
-    first_name: "Katee",
-    last_name: "Gopsall",
-    email: "kgopsallm1@cam.ac.uk",
-    ip_address: "203.138.133.164",
-    latitude: 5.7204203,
-    longitude: 10.901604
-  }
-];
 const londonUsers = [
   {
     id: 135,
@@ -135,37 +52,18 @@ const londonUsers = [
     ip_address: "203.138.133.164",
     latitude: 5.7204203,
     longitude: 10.901604
-  }
-];
-const aroundLondonUsers = [
-  {
-    id: 266,
-    first_name: "Ancell",
-    last_name: "Garnsworthy",
-    email: "agarnsworthy7d@seattletimes.com",
-    ip_address: "67.4.69.137",
-    latitude: 51.6553959,
-    longitude: 0.0572553
   },
   {
-    id: 322,
-    first_name: "Hugo",
-    last_name: "Lynd",
-    email: "hlynd8x@merriam-webster.com",
-    ip_address: "109.0.153.166",
-    latitude: 51.6710832,
-    longitude: 0.8078532
-  },
-  {
-    id: 554,
-    first_name: "Phyllys",
-    last_name: "Hebbs",
-    email: "phebbsfd@umn.edu",
-    ip_address: "100.89.186.13",
-    latitude: 51.5489435,
-    longitude: 0.3860497
+    id: 1000,
+    first_name: "Katee1",
+    last_name: "Gopsall1",
+    email: "kgopsallm11@cam.ac.uk",
+    ip_address: "203.138.133.123",
+    latitude: 51.509865,
+    longitude: -0.118092
   }
 ];
+
 const allUsers = [
   {
     id: 1,
@@ -229,11 +127,18 @@ const allUsers = [
     ip_address: "100.89.186.13",
     latitude: 51.5489435,
     longitude: 0.3860497
+  },
+  {
+    id: 1000,
+    first_name: "Katee1",
+    last_name: "Gopsall1",
+    email: "kgopsallm11@cam.ac.uk",
+    ip_address: "203.138.133.123",
+    latitude: 51.509865,
+    longitude: -0.118092
   }
 ];
 module.exports = {
-  inAndAroundLondonUsers,
   londonUsers,
-  aroundLondonUsers,
   allUsers
 };
